34d486519f6731c331d7a92a9a54970633a9e1fa
jens <jens@lea.io>
2015-11-29 10:56:48 +0100

    ADding doc tests

--- projects/adder/src/lib.rs
+++ projects/adder/src/lib.rs
@@ -1,3 +1,20 @@
+//! The `adder` crate provides functions that add numbers to other numbers
+//!
+//! # Examples
+//!
+//! ```
+//! assert_eq!(4, adder::add_two(2));
+//! ```
+
+/// This function adds two to its argument
+///
+///.DocumentRevisions-V100 # Examples
+///
+/// ```
+/// use adder::add_two;
+///
+/// assert_eq!(4, add_two(2));
+/// ```
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
@@ -10,11 +27,6 @@ mod tests {
     #[should_panic(expected = "assertion failed")]
     fn it_works() {
         assert_eq!("Hello","World");
-    }
-
-    #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
     }
 
     #[test]
