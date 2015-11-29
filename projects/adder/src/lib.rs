6197d98eb43d5b26cc09e2c7688432461340c8ed
jens <jens@lea.io>
2015-11-29 10:35:43 +0100

    My first tests

---
+++ projects/adder/src/lib.rs
@@ -0,0 +1,22 @@
+#[test]
+#[should_panic(expected = "assertion failed")]
+fn it_works() {
+    assert_eq!("Hello","World");
+}
+
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, add_two(2));
+}
+
+
+#[test]
+#[ignore]
+fn expensive_test() {
+    // will and should be ignored in standard test runs
+}
